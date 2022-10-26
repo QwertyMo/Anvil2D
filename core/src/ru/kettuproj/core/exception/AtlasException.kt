640ec4bcf3e43c97187cc500be7dfa373f34c399
QwertyMo <qwertymo26@gmail.com>
2022-10-26 11:11:13 +0300

    Atlas manager documentation

---
+++ core/src/ru/kettuproj/core/exception/AtlasException.kt
@@ -0,0 +1,8 @@
+package ru.kettuproj.core.exception
+
+/**
+ * Exception, called when atlas initialized wrongly
+ *
+ * @author QwertyMo
+ */
+class AtlasException(message: String) : Exception(message)
