7a13ebfdd10e12893347f97e0a4f802d15c7d40d
QwertyMo <qwertymo26@gmail.com>
2022-11-20 12:05:02 +0300

    Unpack and search discord dlls

--- core/src/ru/kettuproj/core/connections/discord/DiscordAPI.kt
+++ core/src/ru/kettuproj/core/connections/discord/DiscordAPI.kt
@@ -3,35 +3,19 @@ package ru.kettuproj.core.connections.discord
 import de.jcm.discordgamesdk.Core
 import de.jcm.discordgamesdk.CreateParams
 import de.jcm.discordgamesdk.activity.Activity
-import org.apache.log4j.Level
-import org.apache.log4j.Logger
+import org.apache.commons.io.FileUtils
+import org.apache.commons.lang3.SystemUtils
 import ru.kettuproj.core.exception.DiscordException
 import java.io.File
-import java.nio.file.FileSystem
-import java.nio.file.FileSystems
 import java.time.Instant
-import kotlin.io.path.copyTo
-import kotlin.io.path.exists
+import kotlin.io.path.*
 
 
 class DiscordAPI {
     var discord: Core? = null
 
     fun initialize(clientId:Long){
-        val path = File(this::class.java.protectionDomain.codeSource.location.toURI()).parentFile.path
-        println(path)
-        Logger.getLogger(this.javaClass.name).log(Level.INFO, "Start search all internal assets")
-        val uri = this::class.java.getResource("/")?.toURI() ?: throw DiscordException("uri is null")
-        var filesystem: FileSystem? = null
-        if(uri.scheme.equals("jar")){
-            filesystem = FileSystems.newFileSystem(uri, mapOf<String, Any>())
-            println(filesystem.getPath("discord").exists())
-            filesystem.getPath("discord").copyTo(File(path).toPath())
-            //Core.init(filesystem.getPath("discord/discord_game_sdk.dll").toFile())
-
-        }else{
-            Core.init(File("C:\\Users\\QwertyMo\\IdeaProjects\\Anvil2D\\core\\src\\main\\resources\\discord\\discord_game_sdk.dll"))
-        }
+        initCore()
         val params = CreateParams()
         params.clientID = clientId
         params.flags = CreateParams.getDefaultFlags()
@@ -41,6 +25,28 @@ class DiscordAPI {
         activity.state = "..."
         activity.timestamps().start = Instant.now()
         discord!!.activityManager().updateActivity(activity)
+    }
+
+    private fun initCore(){
+        val path = File(this::class.java.protectionDomain.codeSource.location.toURI()).parentFile.path
+        val uri = this::class.java.getResource("/discord")?.toURI() ?: throw DiscordException("uri is null")
+        val extension =
+            if(SystemUtils.IS_OS_WINDOWS) ".dll"
+            else if(SystemUtils.IS_OS_LINUX) ".so"
+            else if(SystemUtils.IS_OS_MAC) ".dylib"
+            else return
+        if(uri.scheme.equals("jar")) {
+            val file = File("$path/data/discord_game_sdk${extension}")
+            val dir = File("$path/data")
+            if (!dir.exists()) dir.mkdir()
+            if (!file.exists()) file.createNewFile()
+            val stream = this::class.java.getResourceAsStream("/discord/discord_game_sdk${extension}")
+            FileUtils.copyInputStreamToFile(stream, file)
+            stream?.close()
+            Core.init(file)
+        }else{
+            Core.init(File("${uri.toPath()}/discord_game_sdk${extension}"))
+        }
     }
 
 
