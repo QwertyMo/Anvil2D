bbd97e8a8ba8b893214ee22dee43f73e5cc600f5
QwertyMo <qwertymo26@gmail.com>
2022-11-22 14:29:30 +0300

    Clear and improve discord api

--- core/src/ru/kettuproj/core/connections/discord/DiscordAPI.kt
+++ core/src/ru/kettuproj/core/connections/discord/DiscordAPI.kt
@@ -2,29 +2,26 @@ package ru.kettuproj.core.connections.discord
 
 import de.jcm.discordgamesdk.Core
 import de.jcm.discordgamesdk.CreateParams
-import de.jcm.discordgamesdk.activity.Activity
 import org.apache.commons.io.FileUtils
 import org.apache.commons.lang3.SystemUtils
 import ru.kettuproj.core.exception.DiscordException
 import java.io.File
-import java.time.Instant
 import kotlin.io.path.*
 
 
 class DiscordAPI {
     var discord: Core? = null
+    var initialized = false
+        private set
 
     fun initialize(clientId:Long){
         initCore()
-        val params = CreateParams()
-        params.clientID = clientId
-        params.flags = CreateParams.getDefaultFlags()
-        discord = Core(params)
-        val activity = Activity()
-        activity.details = "Discord test"
-        activity.state = "..."
-        activity.timestamps().start = Instant.now()
-        discord!!.activityManager().updateActivity(activity)
+        if(initialized){
+            val params = CreateParams()
+            params.clientID = clientId
+            params.flags = CreateParams.getDefaultFlags()
+            discord = Core(params)
+        }
     }
 
     private fun initCore(){
@@ -36,16 +33,26 @@ class DiscordAPI {
             else if(SystemUtils.IS_OS_MAC) ".dylib"
             else return
         if(uri.scheme.equals("jar")) {
-            val file = File("$path/data/discord_game_sdk${extension}")
-            val dir = File("$path/data")
-            if (!dir.exists()) dir.mkdir()
-            if (!file.exists()) file.createNewFile()
-            val stream = this::class.java.getResourceAsStream("/discord/discord_game_sdk${extension}")
-            FileUtils.copyInputStreamToFile(stream, file)
-            stream?.close()
-            Core.init(file)
+            initialized = try{
+                val file = File("$path/data/discord_game_sdk${extension}")
+                val dir = File("$path/data")
+                if (!dir.exists()) dir.mkdir()
+                if (!file.exists()) file.createNewFile()
+                val stream = this::class.java.getResourceAsStream("/discord/discord_game_sdk${extension}")
+                FileUtils.copyInputStreamToFile(stream, file)
+                stream?.close()
+                Core.init(file)
+                true
+            }catch (e: Exception){
+                false
+            }
         }else{
-            Core.init(File("${uri.toPath()}/discord_game_sdk${extension}"))
+            initialized = try{
+                Core.init(File("${uri.toPath()}/discord_game_sdk${extension}"))
+                true
+            }catch (e: Exception){
+                false
+            }
         }
     }
 
