6ee06ddb09e884c5faf70321c68c22356f6f605b
QwertyMo <qwertymo26@gmail.com>
2022-11-23 12:09:52 +0300

    DiscordAPI improvements

--- core/src/ru/kettuproj/core/connections/discord/Activity.kt
+++ core/src/ru/kettuproj/core/connections/discord/Activity.kt
@@ -29,14 +29,43 @@ class Activity(private val api: Core) {
             api.activityManager().updateActivity(activity)
         }
 
-    fun show(){
+    var largeImage: String = ""
+        set(value){
+            field = value
+            activity.assets().largeImage = value
+            api.activityManager().updateActivity(activity)
+        }
+
+    var largeImageDescription: String = ""
+        set(value){
+            if(value.length == 1) return
+            field = value
+            activity.assets().largeText = value
+            api.activityManager().updateActivity(activity)
+        }
+
+    var smallImage: String = ""
+        set(value){
+            field = value
+            activity.assets().smallImage = value
+            api.activityManager().updateActivity(activity)
+        }
+
+    var smallImageDescription: String = ""
+        set(value){
+            if(value.length == 1) return
+            field = value
+            activity.assets().smallText = value
+            api.activityManager().updateActivity(activity)
+        }
+
+
+    init{
         activity.state = description
         activity.details = state
         activity.timestamps().start = startTime
+        activity.assets().largeText = largeImageDescription
+        activity.assets().smallText = smallImageDescription
         api.activityManager().updateActivity(activity)
-    }
-
-    init{
-        show()
     }
 }
